55da6f2d487a9efd3a5fdd3ddbf7a12dbcb2f66a
Matan Maron <matanmaron@gmail.com>
2020-07-06 19:08:37 +0300

    again...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io(`https://matanchatapp.herokuapp.com:3000`);
+const socket = io('https://matanchatapp.herokuapp.com:3000');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
