2ffd64a7022adffbd7b8e8fdc0f7870426b3ef05
Matan Maron <matanmaron@gmail.com>
2020-07-06 13:31:57 +0300

    add bad words

--- script.js
+++ script.js
@@ -3,32 +3,47 @@ const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const name = prompt('Name Please?');
-appendMessage('You Joined');
-socket.emit('new-user',name);
+const color = getRandomColor();
+appendMessage('You Joined', color);
+socket.emit('new-user', { name: name, color: color });
 
 socket.on('chat-message', data=>{
-    appendMessage(`${data.name}: ${data.message}`);
+    appendMessage(`${data.user.name}: ${data.message}`, data.user.color);
 })
 
-socket.on('user-connected', name=>{
-    appendMessage(`${name} connected`);
+socket.on('user-connected', user=>{
+    appendMessage(`${user.name} connected`, user.color);
 })
 
-socket.on('user-disconnected', name=>{
-    appendMessage(`${name} disconnected`);
+socket.on('user-disconnected', user=>{
+    appendMessage(`${user.name} disconnected`, user.color);
 })
 
 messageForm.addEventListener('submit', e=>{
     e.preventDefault();
     const message = messageInput.value;
-    appendMessage(`You: ${message}`);
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
+    socket.emit('filter', message);
 });
 
-function appendMessage(message)
+socket.on('filter', message=>{
+    appendMessage(`You: ${message}`, color);
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+})
+
+function appendMessage(message, color)
 {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
+    messageElement.style.background = color;
     messageContainer.append(messageElement);
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const io = require('socket.io')(3000);
+const Filter = require('bad-words');
+filter = new Filter();
 const users = {};
-
 io.on('connection', socket => {
-    socket.on('new-user', name =>{
-        if(name==null)
+    socket.on('new-user', user =>{
+        if(user.name==null)
         {
-            name = "John-Doe";
+            user.name = "John-Doe";
         }
-        users[socket.id] = name;
-        socket.broadcast.emit('user-connected',name);
+        users[socket.id] = user;
+        socket.broadcast.emit('user-connected',user);
     });
     socket.on('send-chat-message', message=>{
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] } );
+        socket.broadcast.emit('chat-message', { message: filter.clean(message), user: users[socket.id] } );
     });
     socket.on('disconnect-user', () =>{
         socket.broadcast.emit('user-disconnected',users[socket.id]);
         delete users[socket.id];
+    });
+    socket.on('filter', message=>{
+        socket.emit('filter', filter.clean(message) );
     });
 });
